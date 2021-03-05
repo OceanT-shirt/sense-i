a3a0f81334eef1f1a62d27b2e8a392b303f95aa7
OceanT-shirt <stylish.trousers@gmail.com>
2021-03-06 08:47:03 +0900

    changed html

--- sense_i/settings.py
+++ sense_i/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'sense_i.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sense_i',
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB.PASSWORD'),
-        'HOST': '',
-        'POST': '',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'sense_i',
+        # 'USER': os.environ.get('DB_USER'),
+        # 'PASSWORD': os.environ.get('DB.PASSWORD'),
+        # 'HOST': '',
+        # 'POST': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # TODO
     }
 }
 
